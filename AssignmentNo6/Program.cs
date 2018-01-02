6b89ef5fafd97f4e93db76ad355284dbfa4fe59a
ahmadmohey <ahmadmohey@gmail.com>
2018-01-02 17:30:08 +0200

    Complete C# - AssignmentNo6 Solution

---
+++ AssignmentNo6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentNo6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            Random rand = new Random();
+            int x = 0;
+            do
+            {
+                Console.WriteLine("Roll dice? (yes, no)");
+                input = Console.ReadLine();
+                if (input.ToLower() == "yes")
+                {
+                    x = rand.Next(1, 7);
+                    Console.WriteLine("New dice no is " + x);
+                }
+            }
+            while(input.ToLower() != "no");
+        }
+    }
+}
